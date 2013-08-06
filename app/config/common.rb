6a71193d01c96740c3dc9a944ffac90c87f4df5b
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2013-08-05 22:47:29 -0300

    Removed debug code

--- app/config/common.rb
+++ app/config/common.rb
@@ -1,7 +1,6 @@
 class Api < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__),'..', '..')
   set :views, File.join(File.dirname(__FILE__), '..', 'views')
-  p public_folder
   register Sinatra::ActiveRecordExtension
 end
 
